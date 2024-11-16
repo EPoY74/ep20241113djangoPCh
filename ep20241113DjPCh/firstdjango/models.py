5748faf5016bb679a625e5e4e1dbeb8c3c91adc9
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-16 12:01:53 +0500

    init2

--- ep20241113DjPCh/firstdjango/models.py
+++ ep20241113DjPCh/firstdjango/models.py
@@ -6,3 +6,6 @@ class Workers(models.Model):
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35)
     salary = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.second_name} {self.name}"
