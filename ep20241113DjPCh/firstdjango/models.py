d590a6d08587a800121db419f8bd693db16a3b14
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-19 09:18:28 +0500

    Рефакторинг кода с учетом пожеланий линтера

--- ep20241113DjPCh/firstdjango/models.py
+++ ep20241113DjPCh/firstdjango/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # # Create your models here.
 
+
 class Workers(models.Model):
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35)
