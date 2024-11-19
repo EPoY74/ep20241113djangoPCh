6926845096747cfc7565c4bed48551e2a035994f
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-19 11:53:55 +0500

    Сохранение

--- ep20241113DjPCh/firstdjango/models.py
+++ ep20241113DjPCh/firstdjango/models.py
@@ -8,5 +8,7 @@ class Workers(models.Model):
     second_name = models.CharField(max_length=35)
     salary = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 
+    objects = models.Manager()
+
     def __str__(self):
         return f"{self.second_name} {self.name}"
--- ep20241113DjPCh/firstdjango/views.py
+++ ep20241113DjPCh/firstdjango/views.py
@@ -1,5 +1,25 @@
+"""
+Представление страниц
+Учебный проект
+Автор: Евгений Петров
+Почта: p174@mail.ru
+Урок: https://www.youtube.com/watch?v=KH3eobiFhGA&t=1179s
+"""
+
+
 from django.shortcuts import render
+from firstdjango.models import Workers
+
 
 # Create your views here.
 def index_page(request):
+    """
+    Вывод основной страницы в браузер
+    :param request:
+    :return:
+    """
+
+    all_workers = Workers.objects.all()
+    for worker in all_workers:
+        print(f"{worker}")
     return render(request, "index.html")
