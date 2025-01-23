8a3c58ec9ddc61b02db5e882f6a8da78b32df543
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-23 09:29:06 +0500

    Добавил  докстринги и сделал преобразование типа, так как ругался Линтер

--- ep20241113DjPCh/firstdjango/models.py
+++ ep20241113DjPCh/firstdjango/models.py
@@ -1,12 +1,29 @@
+"""
+    Модели для сохранения данных 
+
+    Returns:
+        _type_: _description_
+"""
+
+from decimal import Decimal
+
 from django.db import models
+
 
 # # Create your models here.
 
 
 class Workers(models.Model):
+    """Модель данных, описывающая работника
+    Args:
+        models (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35)
-    salary = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    salary = models.DecimalField(max_digits=8, decimal_places=2, default=Decimal(0))
 
     objects = models.Manager()
 
